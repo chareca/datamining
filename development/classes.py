77e015a7b558ff078229aacb93efc2546a2e4d70
Nicolás Chareca <55698485+chareca@users.noreply.github.com>
2026-03-12 13:40:49 +0100

    feat: initial model class development

--- development/classes.py
+++ development/classes.py
@@ -1,5 +1,4 @@
-""" Clases utilizadas en los demás archivos """
-import pandas as pd
+"""Clases utilizadas en los demás archivos"""
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
@@ -7,21 +6,31 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.compose import ColumnTransformer
 
-class OutlierDetector(TransformerMixin, BaseEstimator):
-	""" Detector y corrector de outliers por la mediana """
+class OutlierDetector(TransformerMixin, BaseEstimator): # TODO: Generalizar a más metodos
+	"""Detector y corrector de outliers por la mediana"""
 	def __init__(self, k: float | None = 1.5):
 		self.stats = None
 		self.columns = None
 		self.k = k
 
 	def fit(self, X: pd.DataFrame, y: pd.DataFrame | None = None):
+		if isinstance(X, np.ndarray):
+			X = pd.DataFrame(X)
+		if isinstance(y, np.ndarray):
+			y = pd.DataFrame(y)
+		
 		self.stats = X.describe()
 		self.columns = X.columns
 		return self
 
 	def transform(self, X: pd.DataFrame):
-		Xaux = X.copy()
+		if isinstance(X, np.ndarray):
+			Xaux = pd.DataFrame(X).copy()
+		else:
+			Xaux = X.copy()
 
 		for column in self.columns:
 			Q1 = self.stats[column]["25%"]
@@ -39,12 +48,15 @@ class OutlierDetector(TransformerMixin, BaseEstimator):
 
 		return Xaux
   
-class Model():
-	def __init__(self, clasificador, imputador, transformador, estandarizador = None):
-		self.clasificador = clasificador
-		self.estandarizador = estandarizador
+class Model(): # TODO: Agregar ColumnTransformer
+	def __init__(self, imputador, transformador, clasificador, estandarizador = None):
 		self.imputador = imputador
 		self.transformador = transformador
+		self.estandarizador = estandarizador
+		self.clasificador = clasificador
+		self.param_grid = None
+		self.scorer = None
+		self.modelo = None
 
 	def create_pipeline(self):
 		if self.estandarizador is not None:
@@ -57,4 +69,47 @@ class Model():
 					("transformador", self.transformador),
 					("modelo", self.clasificador)])
 		return pipe
+		
+	def set_params(self, param_grid: dict) -> None:
+		# Verificación datos válidos del param_grid
+		if isinstance(param_grid, list):
+			for params in param_grid:
+				for key in params.keys():
+					if key.split("__")[0] not in ["imputador", "transformador", "estandarizador", "modelo"]:
+						raise ValueError("[ERROR]: El diccionario de parametros introducido utiliza nombres inválidos.\nUsar: 'imputador', 'transformador', 'estandarizador' o 'modelo'.")
+		else:
+			for key in param_grid.keys():
+				if key.split("__")[0] not in ["imputador", "transformador", "estandarizador", "modelo"]:
+					raise ValueError("[ERROR]: El diccionario de parametros introducido utiliza nombres inválidos.\nUsar: 'imputador', 'transformador', 'estandarizador' o 'modelo'.")
+		self.param_grid = param_grid
+
+	def set_scorer(self, scorer):
+		self.scorer = scorer
+
+	def fit(self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.DataFrame | None = None):
+		"""Devuelve el modelo entrenado."""
+		
+		pipe = self.create_pipeline()
+		self.modelo = pipe
+
+		if self.param_grid is not None:
+			grid = GridSearchCV(pipe, self.param_grid, scoring=self.scorer)
+			self.modelo = grid
+		
+		self.modelo = self.modelo.fit(X, y)
+		return self.modelo
+		
+	def predict(self, X: np.ndarray | pd.DataFrame) -> np.ndarray | pd.DataFrame:
+		return self.modelo.predict(X)
+
+	def accuracy_score(self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.DataFrame, decimals: int | None = 4) -> float:
+		predicciones = self.predict(X)
+		return round(accuracy_score(y, predicciones)*100, decimals)
 	
+	def f1_score(self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.DataFrame, decimals: int | None = 4) -> float:
+		predicciones = self.predict(X)
+		return round(f1_score(y, predicciones)*100, decimals)
+		
+	def confusion_matrix(self, X: np.ndarray | pd.DataFrame, y: np.ndarray | pd.DataFrame, labels: list | None = None) -> np.ndarray:
+		predicciones = self.predict(X)
+		return confusion_matrix(y, predicciones, labels)
