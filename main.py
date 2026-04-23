e1da32f941533ff1435438aedc9e793e865fee4b
Nicolás Chareca <55698485+chareca@users.noreply.github.com>
2026-04-23 09:18:43 +0200

    feat: add graphs before predictions

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-"""
-Hito 1 — Primera solución automatizada: ColumnTransformer + Pipeline + KNN
-Métrica: AUC-ROC      Validación: StratifiedKFold(10)
-"""
-
-"""
-1. Imputacion valores perdidos
-2. Transformación categóricas -> numéricas
-3. (dsp 2do hito) estandarizar
-4. KNN (en principio parámetros fijos)
-"""
-
-from classes import OutlierDetector, Preparador, Preparador2, Preparador3, Model, Imputer, Transformador
+from classes import Preparador3, Model, Imputer, Transformador
 from sklearn.neighbors import KNeighborsClassifier
-import numpy as np
-import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from functions_scripts.load import read_data
-from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+import pandas as pd                                                                                               
+
+"""
+    Hito 1 — Primera solución automatizada: ColumnTransformer + Pipeline + KNN
+    Métrica: Accuracy      Validación: StratifiedKFold(10)
+
+    1. Imputacion valores perdidos
+    2. Transformación categóricas -> numéricas
+    3. (dsp 2do hito) estandarizar
+    4. KNN (en principio parámetros fijos)
+"""
+
+
+def get_graphs(X, y):
+    fig, axes = plt.subplots(2, 5, figsize=(15, 6))
+    axes = axes.ravel()
+
+    for i in range(10):
+        col = X.columns[i]
+        ct = pd.crosstab(X[col], y) # filas: agree/disagree, cols: 0/1                                   
+        ct.columns = ["no", "yes"]
+        ct.plot(kind="bar", ax=axes[i], legend=(i == 0))
+        axes[i].set_title(f"Pregunta {i+1}")
+        axes[i].set_xlabel("")
+                
+    fig.suptitle("Relaciones entre preguntas y clase final")                                                      
+    fig.tight_layout()
+    plt.show()
+
+    fig, axes = plt.subplots(2,2, figsize=(15, 6))
+    axes = axes.ravel()
+    fig.suptitle("Relaciones entre características y clase final")
+    columns = ["jaundice", "family_pdd", "used_app_before", "relation"]
+    for i in range(4):
+        col = columns[i]
+        ct = pd.crosstab(X[col], y)                                   
+        ct.columns = ["no", "yes"]
+        ct.plot(kind="bar", ax=axes[i], legend=(i == 0))
+        axes[i].set_title(f"{col}")
+        axes[i].set_xlabel("")
+
+    plt.show()
 
 def main():
     # Configuración
@@ -31,7 +59,10 @@ def main():
     # Creamos los conjuntos de datos
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1].cat.codes.astype(int)   # no=0, yes=1
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
+
+    get_graphs(X, y)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed, stratify=y)
 
     modelo = Model(
         imputador=Imputer(),
@@ -49,7 +80,7 @@ def main():
         "modelo__n_jobs": [-1],
     }
     modelo.set_params(param_grid)
-    modelo.set_scorer("roc_auc")
+    modelo.set_scorer("accuracy")
 
     modelo.fit(X_train, y_train)
     
