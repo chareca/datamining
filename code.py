44f674141420a13420c9769a870aba5b516482f1
Juan Felipe <juancho@MacBook-Pro-de-Juan.local>
2026-05-13 22:21:19 +0200

    final de los finales, el final se acerca

---
+++ code.py
@@ -0,0 +1,2 @@
+import pandas as pd
+# I'll just check X_resto variables in notebook execution
---
+++ test_script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import pickle
+import warnings
+warnings.filteraxis = lambda *a, **k: None
+warnings.simplefilter("ignore")
+
+# Since pipeline is in memory of notebook, let's write a script to check it using notebook variables.
